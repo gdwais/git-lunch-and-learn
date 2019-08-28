87175ee5bba70cf21baa360486c4d5a5c96962ba
Dalton Wais <georgedaltonwais@gmail.com>
2019-08-28 11:22:24 -0600

    adding get endpoint

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const pkg = require("./package.json");
 
+app.get('/ping', (req, res) => {
+
+});
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
